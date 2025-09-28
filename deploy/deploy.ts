5c61a9dfea6a3803204f2acf0f61254ffea4c954
fairfaxtech <hamiyejunxi@hotmail.com>
2025-09-28 14:20:21 +0800

    deploy contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedRememberPass = await deploy("RememberPass", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`RememberPass contract: `, deployedRememberPass.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_rememberPass"; // id required to prevent reexecution
+func.tags = ["RememberPass"];
