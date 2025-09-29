1885313dfa50b68b273d2c8196e98bd80fc894fe
fairfaxtech <hamiyejunxi@hotmail.com>
2025-09-29 20:55:27 +0800

    sepolia fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,8 @@ import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 import "solidity-coverage";
 
 import "./tasks/accounts";
@@ -16,7 +18,9 @@ import "./tasks/RememberPass";
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
 const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY || vars.get("INFURA_API_KEY", "");
+const PRIVATE_KEY_ENV = process.env.PRIVATE_KEY || ""; // without 0x in .env
+const PRIVATE_KEY = PRIVATE_KEY_ENV ? (PRIVATE_KEY_ENV.startsWith("0x") ? PRIVATE_KEY_ENV : `0x${PRIVATE_KEY_ENV}`) : "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -50,7 +54,7 @@ const config: HardhatUserConfig = {
       url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : {
         mnemonic: MNEMONIC,
         path: "m/44'/60'/0'/0/",
         count: 10,
